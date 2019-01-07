d3bbb6d72f61faaae5fdfa746fc1ac4ba8c7180d
Habbey kumapayi <habbeyunik@gmail.com>
2019-01-07 10:47:10 +0100

    created typography components

---
+++ src/components/typography/index.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const Body = styled.p`
+  font-family: San-Francisco-Regular;
+  font-size: 1em;
+  letter-spacing: 0.5px;
+  line-height: ${1.5 * 1}em;
+`;
+export const Subheading = styled.p`
+  font-family: San-Francisco-Regular;
+  font-size: 1.3em;
+  letter-spacing: 0.5px;
+  line-height: ${1.5 * 1.3}em;
+`;
+export const Headline = styled.p`
+  font-family: San-Francisco-Regular;
+  font-size: 1.6em;
+  letter-spacing: 0.5px;
+  line-height: ${1.5 * 1.6}em;
+`;
+
+export const Caption = styled.span`
+  font-family: San-Francisco-Regular;
+  font-size: 0.769em;
+  opacity: 0.7;
+  letter-spacing: 0.25px;
+  line-height: ${1.5 * 0.769}em;
+`;
+
+export const H1 = styled.h1`
+  font-family: San-Francisco-Regular;
+  font-size: 4.827em;
+  letter-spacing: -0.5px;
+  line-height: ${1.5 * 4.827}em;
+`;
+
+export const H2 = styled.h2`
+  font-family: San-Francisco-Regular;
+  font-size: 3.713em;
+  letter-spacing: -0.25px;
+  line-height: ${1.5 * 3.713}em;
+`;
+
+export const H3 = styled.h3`
+  font-family: San-Francisco-Regular;
+  font-size: 2.856em;
+  letter-spacing: -0.25px;
+  line-height: ${1.5 * 2.856}em;
+`;
