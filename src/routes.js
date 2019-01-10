c349d273b719c55f7757c645c9dbb7fa7345eea8
Habbey kumapayi <habbeyunik@gmail.com>
2019-01-10 15:16:39 +0100

    about page done

--- src/routes.js
+++ src/routes.js
@@ -16,12 +16,20 @@ const LoadableGallery = Loadable({
   }
 });
 
+const LoadableAbout = Loadable({
+  loader: () => import(/* webpackChunkName: 'home' */ './pages/about'),
+  loading() {
+    return <div>Loading...</div>;
+  }
+});
+
 export default function Routes() {
   return (
     <div className="main">
       <Switch>
         <Route exact path="/" component={LoadableHome} />
         <Route path="/gallery" component={LoadableGallery} />
+        <Route path="/about" component={LoadableAbout} />
       </Switch>
     </div>
   );
