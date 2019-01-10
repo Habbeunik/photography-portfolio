07f4f5617f69fc48c5227e34d23daeba6d8198eb
Habbey kumapayi <habbeyunik@gmail.com>
2019-01-10 22:12:19 +0100

    removed css module

--- src/components/social-handles/index.js
+++ src/components/social-handles/index.js
@@ -1,37 +1,20 @@
 import React from 'react';
 import IconButton from '../button/icon';
-import styled from 'styled-components';
 import { Facebook, Instagram, Twitter } from 'react-feather';
-
-const Container = styled.div`
-  position: fixed;
-  top: 30vh;
-  left: 8%;
-  z-index: 4;
-`;
-
-const FacebookIcon = styled(Facebook)`
-  fill: #3b5998;
-  color: #3b5998;
-`;
-
-const TwitterIcon = styled(Twitter)`
-  fill: #00aced;
-  color: #00aced;
-`;
+import * as styles from './handlers.scss';
 
 export default function SocialHanldes() {
   return (
-    <Container>
+    <div className={styles.container}>
       <a href="https://www.facebook.com/ikumapayi.abbey" target="#">
-        <IconButton icon={<FacebookIcon size={25} />} />
+        <IconButton icon={<Facebook className={styles.facebook} size={25} />} />
       </a>
       <a href="https://www.twitter.com/habbeyunik" target="#">
-        <IconButton icon={<TwitterIcon size={25} />} />
+        <IconButton icon={<Twitter className={styles.twitter} size={25} />} />
       </a>
       <a href="https://www.instagram.com/evergreenfella/" target="#">
         <IconButton icon={<Instagram size={30} />} />
       </a>
-    </Container>
+    </div>
   );
 }
