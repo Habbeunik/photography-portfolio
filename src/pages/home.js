b530ecf99c83260f9d713630725bcef6c6ce5cf2
Habbey kumapayi <habbeyunik@gmail.com>
2019-01-10 19:17:18 +0100

    added home page introductory text and background images

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,35 +1,64 @@
 import React from 'react';
-import styled from 'styled-components';
-// import ImageThumbnail from '../components/image-thumbnail';
+import { H3, Subheading } from '../components/typography';
 
-const ImageBoardImage = styled.img`
-  margin: auto;
-  height: auto;
-  width: auto;
-  display: block;
-`;
+export default class Home extends React.Component {
+  state = {
+    images: [
+      'https://images.pexels.com/photos/1371176/pexels-photo-1371176.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/1389102/pexels-photo-1389102.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/1029929/pexels-photo-1029929.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/1751196/pexels-photo-1751196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/1741235/pexels-photo-1741235.jpeg?cs=srgb&dl=adorable-animal-beagle-1741235.jpg&fm=jpg',
+      'https://images.pexels.com/photos/67386/pexels-photo-67386.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/1038041/pexels-photo-1038041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/1340502/pexels-photo-1340502.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/834872/pexels-photo-834872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/325807/pexels-photo-325807.jpeg?cs=srgb&dl=adventure-beautiful-daylight-325807.jpg&fm=jpg'
+    ],
+    currentImageIndex: 0
+  };
 
-export default function Home() {
-  return (
-    <div className="body">
-      <div className={'image-board'}>
-        <ImageBoardImage src="https://images.pexels.com/photos/219552/pexels-photo-219552.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+  setImage = index => {
+    this.setState({ currentImageIndex: index });
+  };
+
+  manageImageChange = () => {
+    const { currentImageIndex, images } = this.state;
+    if (currentImageIndex < images.length - 1) {
+      this.setImage(currentImageIndex + 1);
+    } else {
+      this.setImage(0);
+    }
+  };
+
+  getCurrentImage = () => {
+    return this.state.images[this.state.currentImageIndex];
+  };
+  componentDidMount = () => {
+    this.interval = setInterval(this.manageImageChange, 8000);
+  };
+  componentWillUnmount = () => {
+    clearInterval(this.interval);
+  };
+
+  render() {
+    return (
+      <div className="body">
+        <div
+          className={'image-board'}
+          style={{
+            background: '#f2f2f2',
+            backgroundImage: `url(${this.getCurrentImage()})`
+          }}
+        >
+          <div className="introduction-textContainer">
+            <H3 className="introduction-title">
+              Kumapayi <span>Habbey</span>
+            </H3>
+            <Subheading>I&apos;m a Photographer</Subheading>
+          </div>
+        </div>
       </div>
-      {/* <div
-        style={{ display: 'flex', justifyContent: 'center', marginTop: '15px' }}
-      >
-        <ImageThumbnail image="https://images.pexels.com/photos/1209843/pexels-photo-1209843.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <ImageThumbnail image="https://images.pexels.com/photos/325044/pexels-photo-325044.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <ImageThumbnail image="https://images.pexels.com/photos/417040/pexels-photo-417040.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <ImageThumbnail image="https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <ImageThumbnail image="https://images.pexels.com/photos/545008/pexels-photo-545008.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <ImageThumbnail image="https://images.pexels.com/photos/356147/pexels-photo-356147.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        <ImageThumbnail
-          isActive={true}
-          image="https://images.pexels.com/photos/219552/pexels-photo-219552.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        />
-        <ImageThumbnail image="https://images.pexels.com/photos/458381/pexels-photo-458381.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-      </div> */}
-    </div>
-  );
+    );
+  }
 }
