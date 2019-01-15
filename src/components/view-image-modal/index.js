0d32a446f0cf099f140ec27cc38495c85e495008
Habbey kumapayi <habbeyunik@gmail.com>
2019-01-15 19:32:35 +0100

    mobile responsivityi

--- src/components/view-image-modal/index.js
+++ src/components/view-image-modal/index.js
@@ -2,180 +2,114 @@ import React from 'react';
 import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 import { ChevronLeft, ChevronRight } from 'react-feather';
+import { photos } from '../../pages/gallery';
+import viewport from '../../utils/viewport';
 
-export const photos = [
-  {
-    src:
-      'https://images.pexels.com/photos/1371176/pexels-photo-1371176.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/1389102/pexels-photo-1389102.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/1029929/pexels-photo-1029929.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/1751196/pexels-photo-1751196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/1741235/pexels-photo-1741235.jpeg?cs=srgb&dl=adorable-animal-beagle-1741235.jpg&fm=jpg'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/67386/pexels-photo-67386.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/219552/pexels-photo-219552.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/1209843/pexels-photo-1209843.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/1038041/pexels-photo-1038041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/325807/pexels-photo-325807.jpeg?cs=srgb&dl=adventure-beautiful-daylight-325807.jpg&fm=jpg'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/834872/pexels-photo-834872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  },
-  {
-    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599'
-  },
-  {
-    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799'
-  },
-  {
-    src:
-      'https://images.pexels.com/photos/458381/pexels-photo-458381.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-  },
-  {
-    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799'
-  },
-  {
-    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799'
-  },
-  {
-    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799'
-  },
-  {
-    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599'
-  },
-  {
-    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799'
-  },
-  {
-    src: 'https://source.unsplash.com/PpOHJezOalU/800x599'
-  },
-  {
-    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599'
-  },
-  {
-    src: 'https://source.unsplash.com/XiDA78wAZVw/600x799'
-  },
-  {
-    src: 'https://source.unsplash.com/x8xJpClTvR0/800x599'
-  },
-  {
-    src: 'https://source.unsplash.com/qGQNmBE7mYw/800x599'
-  },
-  {
-    src: 'https://source.unsplash.com/NuO6iTBkHxE/800x599'
-  },
-  {
-    src: 'https://source.unsplash.com/A-fubu9QJxE/800x533'
-  },
-  {
-    src: 'https://source.unsplash.com/A-fubu9QJxE/800x533'
-  },
-  {
-    src: 'https://source.unsplash.com/5P91SF0zNsI/740x494'
+const mobileBreakPoint = 500;
+
+const getModalContentStyle = windowWidth => {
+  console.log('window width', windowWidth);
+  console.log('called get modal style function');
+  if (windowWidth <= mobileBreakPoint) {
+    return {
+      width: '90%',
+      height: '90vh',
+      top: '10vh',
+      zIndex: 5,
+      overflow: 'visible',
+      left: '5%',
+      boxSizing: 'border-box'
+    };
   }
-];
+  return {
+    width: '75%',
+    height: '90vh',
+    top: '5vh',
+    zIndex: 5,
+    overflow: 'visible',
+    left: '12.5%',
+    boxSizing: 'border-box'
+  };
+};
 
-export default function ImageModal(props) {
-  const { image, onClose, isOpen, onNext, onPrevious } = props;
-  return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      style={{
-        content: {
-          width: '75%',
-          height: '90vh',
-          top: '5vh',
-          zIndex: 5,
-          overflow: 'visible',
-          left: '12.5%',
-          boxSizing: 'border-box'
-        },
-        overlay: { zIndex: '4', backgroundColor: 'rgba(103, 100, 100, 0.75)' }
-      }}
-    >
-      <div
+export default class ImageModal extends React.Component {
+  state = {
+    modalContentStyle: {}
+  };
+  componentDidMount() {
+    const { width: currentWindowWidth } = viewport();
+    this.setState({
+      modalContentStyle: getModalContentStyle(currentWindowWidth)
+    });
+  }
+  render() {
+    const { image, onClose, isOpen, onNext, onPrevious } = this.props;
+    console.log('modal content style', this.state.modalContentStyle);
+    return (
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onClose}
         style={{
-          width: '100%',
-          boxSizing: 'border-box',
-          overflow: 'hidden',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100%'
+          content: this.state.modalContentStyle,
+          overlay: { zIndex: '4', backgroundColor: 'rgba(103, 100, 100, 0.75)' }
         }}
       >
-        <img
-          style={{ width: 'auto', height: ' 100%' }}
-          src={photos[image].src}
-          alt={image}
-        />
-      </div>
-      {parseInt(image) > 0 && (
+        <div
+          style={{
+            width: '100%',
+            boxSizing: 'border-box',
+            overflow: 'hidden',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%'
+          }}
+        >
+          <img
+            style={{ width: 'auto', height: ' 100%' }}
+            src={photos[image].src}
+            alt={image}
+          />
+        </div>
+        {parseInt(image) > 0 && (
+          <div
+            style={{
+              position: 'absolute',
+              top: '45vh',
+              left: '-75px',
+              outline: 'none',
+              cursor: 'pointer'
+            }}
+            role="button"
+            onClick={onPrevious}
+            onKeyPress={e => {
+              if (e.keyCode == 37) onPrevious();
+            }}
+            tabIndex={0}
+          >
+            <ChevronLeft size={80} color={'#00000096'} />
+          </div>
+        )}
         <div
           style={{
             position: 'absolute',
             top: '45vh',
-            left: '-75px',
+            right: '-75px',
             outline: 'none',
             cursor: 'pointer'
           }}
-          role="button"
-          onClick={onPrevious}
-          onKeyPress={e => {
-            if (e.keyCode == 37) onPrevious();
+          onClick={onNext}
+          onKeyDown={e => {
+            if (e.keyCode === 39) onNext();
           }}
+          role="button"
           tabIndex={0}
         >
-          <ChevronLeft size={80} color={'#00000096'} />
+          <ChevronRight size={80} color={'#00000096'} />
         </div>
-      )}
-      <div
-        style={{
-          position: 'absolute',
-          top: '45vh',
-          right: '-75px',
-          outline: 'none',
-          cursor: 'pointer'
-        }}
-        onClick={onNext}
-        onKeyDown={e => {
-          if (e.keyCode === 39) onNext();
-        }}
-        role="button"
-        tabIndex={0}
-      >
-        <ChevronRight size={80} color={'#00000096'} />
-      </div>
-    </Modal>
-  );
+      </Modal>
+    );
+  }
 }
 
 ImageModal.propTypes = {
